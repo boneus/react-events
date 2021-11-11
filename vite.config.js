c16c8dbb412723e3983ba50d4474de745f814dae
Stanislav Tiptsov <4terion@gmail.com>
2021-11-11 20:06:09 +0300

    Init React app using ViteJS. Add dependencies

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@router': path.resolve(__dirname, './src/router'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@apis': path.resolve(__dirname, './src/apis'),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
