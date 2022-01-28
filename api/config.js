77c9c280c3fe90988d7b37db06807c24f6b0ad01
Stanislav Tiptsov <4terion@gmail.com>
2022-01-28 10:22:03 +0300

    Server: add a delay to the API

--- api/config.js
+++ api/config.js
@@ -2,5 +2,6 @@ module.exports = {
   port: process.env.PORT || 3001,
   host: '0.0.0.0',
   watch: true,
-  routes: 'routes.json'
+  routes: 'routes.json',
+  delay: 1000,
 };
--- api/src/index.js
+++ api/src/index.js
@@ -1,17 +1,23 @@
 const path = require('path');
 const jsonServer = require('json-server');
+const connectPause = require('connect-pause');
+
+const config = require('../config');
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
+server.use(connectPause(config.delay));
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
 server.use('/events/by-user', (req, res) => {
   const events = router.db.getState().events;
   let {user} = req.query;
-  const userEvents = events.filter(event => event.author === user || event.guest === user);
+  const userEvents = events.filter(
+    (event) => event.author === user || event.guest === user
+  );
   res.json(userEvents);
 });
 
